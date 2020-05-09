18228edcd9522bc6326b9e58c9d7c41abdd762e0
Watto <sriraamwatto33@gmail.com>
2020-05-09 12:56:50 +0530

    Completed MongoDB section

--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-prac', 'root', 'root', {
-	dialect: 'mysql',
-	host: 'localhost',
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = callback => {
+	MongoClient.connect(
+		'mongodb+srv://watto:DJC@TJf55YnRXSy@node-prac-urvyj.gcp.mongodb.net/shop?retryWrites=true&w=majority'
+	)
+		.then(client => {
+			_db = client.db();
+			console.log('Connected');
+			callback();
+		})
+		.catch(err => {
+			console.log(err);
+			throw err;
+		});
+};
+
+const getDb = () => {
+	if (_db) return _db;
+	else throw 'No Databases Found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
